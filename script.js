3e2fc96167940ea30575c37c86df0165ef236944
Aylin DeBruyne <adebruyne88@gmail.com>
2018-05-31 13:57:52 -0400

    created Lat and Lng variables to pass into URL

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 // HIKING Project API
 
-function trailInfo(event){
+function trailInfo(location){
+//pull out lat and long into var
+var latitude = location.lat;
+var longitude = location.lng;
+//pass into URL
 
 
-
-var URL = 'https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200279512-8622afc34220d6d5684c57d9ed7c0eb5'
+var URL = `https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=10&key=200279512-8622afc34220d6d5684c57d9ed7c0eb5`
 
 
 $.get(URL)
 
 .then(function(response){
-    console.log(response);
+    // console.log(response);
     //grab trails from respinse
     var trails = response.trails;
-    console.log(trails);
+  
     //create a `div` container
     var container = $(`<div>`, {class: `container`});
 
@@ -39,43 +42,16 @@ $.get(URL)
         trailCard.append(difficultyItem);
         trailCard.append(starsItem);
 
-    // //name of trail
-    // var name = trails[0].name;
-    // var element = $('<p>', {text: `Trail Name: ${name}`});
-
-    // //location
-    // var location = trails[0].location;
-    // var element2 = $('<p>', {text: `Location: ${location}`});
-
-   
-    // //length of trail
-    // var length = trails[0].length;
-    // var element3 = $('<p>', {text: `Length: ${length} miles`});
-
-    // //difficulty
-    // var difficulty =  trails[0].difficulty;
-    // var element4 = $('<p>', {text: `Difficulty: ${difficulty}`})
-    
-    // //stars
-    // var stars = trails[0].stars;
-    // var element5 = $('<p>', {text: `Rating: ${stars} stars`})
-
-    //output to app
-    // $(document.body).append(element);
-    // $(document.body).append(element2);
-    // $(document.body).append(element3); 
-    // $(document.body).append(element4);
-    // $(document.body).append(element5);
-
 });
 
-$(document.body).append(container);
+    //append container to body of HTML
+    $(document.body).append(container);
 });
 
 
 
 }
-
+//call the function
 trailInfo();
 
 
