9fc473b1242145d0145ffbc005bc68e095bb1dad
Aylin DeBruyne <adebruyne88@gmail.com>
2018-06-01 16:05:16 -0400

    made it possible to upload img from API information

--- trail-offers.js
+++ trail-offers.js
@@ -23,21 +23,28 @@ function display(trails){
         var container = $(`<div>`, {class: `container`});
 
         trails.forEach(function(trail){
+        var trailCardDiv = $(`<div>`);
+
         //create an UL for each item on array
         var trailCard = $(`<ul>`);
-        container.append(trailCard);
+        container.append(trailCardDiv);
+        
+        trailCardDiv.append(trailCard);
 
-
+    
         //create list items for each UL
+
         var nameItem = $(`<li>`,{text:`Trail Name: ${trail.name} ` });
         var locationItem = $(`<li>`, {text: `Location: ${trail.location}`});
         var lengthItem = $(`<li>`, {text: `Length: ${trail.length} miles`});
         var difficultyItem = $(`<li>`, {text: `Difficulty: ${trail.difficulty}`});
         var starsItem = $(`<li>`, {text: `${trail.stars} stars`});
-
-
-
+        //pull out image
+         var img = $(`<img>`);  
+         img.attr('src', trail.imgMedium);
+         trailCardDiv.append(img);       
         //appends li to ul
+     
         trailCard.append(nameItem);
         trailCard.append(locationItem);
         trailCard.append(lengthItem);
@@ -45,6 +52,7 @@ function display(trails){
         trailCard.append(starsItem);
 
     });
+
 
     //append container to body of HTML
     $(document.body).append(container);
